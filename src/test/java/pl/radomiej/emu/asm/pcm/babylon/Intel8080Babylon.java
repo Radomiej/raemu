b604449f456d5e8f6f2244d4fea4f405be9be812
rbojba <radoslaw.bojba@itti.com.pl>
2019-10-14 18:11:34 +0200

    start implementation of parser for I8080

--- src/test/java/pl/radomiej/emu/asm/pcm/babylon/Intel8080Babylon.java
+++ src/test/java/pl/radomiej/emu/asm/pcm/babylon/Intel8080Babylon.java
@@ -1,18 +1,40 @@
 package pl.radomiej.emu.asm.pcm.babylon;
 
+import pl.radomiej.emu.cpu.OptcodeFactory;
+import pl.radomiej.emu.cpu.PmcCPU;
+import pl.radomiej.emu.cpu.optcodes.pmc.program.JumpDirectPmc;
 import pl.radomiej.emu.logic.Optcode;
 import pl.radomiej.emu.logic.pure.PureByte;
 
-public class Intel8080Babylon<PmcCPU> {
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Intel8080Babylon implements OptcodeFactory<PmcCPU> {
 
     public PureByte compile(Optcode<PmcCPU> optcode) {
 
         return null;
     }
 
-    public Optcode<PmcCPU> decompile(PureByte machineCode) {
+    @Override
+    public Optcode<PmcCPU> createOptcodeFromMachineCode(PureByte rawInstruction) {
         return null;
     }
 
+    public Optcode<PmcCPU> createOptcodeFromMachineCode(LinkedList<PureByte> pureStackBin) {
+        PureByte commandByte = pureStackBin.poll();
+        String textCommand = commandByte.toBinaryString();
 
+        if(textCommand.equals("11000011")) return jumpInstruction(pureStackBin);
+
+        throw new UnsupportedOperationException("Given command is not supported: " + textCommand);
+    }
+
+    private Optcode<PmcCPU> jumpInstruction(LinkedList<PureByte> pureStackBin) {
+        PureByte lb = pureStackBin.poll();
+        PureByte hb = pureStackBin.poll();
+
+        //TODO add support for duple cell mode
+        return new JumpDirectPmc(0);
+    }
 }
--- src/test/java/pl/radomiej/emu/tests/Intel8080BoardTest.java
+++ src/test/java/pl/radomiej/emu/tests/Intel8080BoardTest.java
@@ -1,15 +1,43 @@
 package pl.radomiej.emu.tests;
 
+import com.google.common.io.Resources;
 import org.junit.jupiter.api.Test;
+import pl.radomiej.emu.asm.pcm.babylon.Intel8080Babylon;
 import pl.radomiej.emu.asm.pure.factory.IncrementAsmExample;
 import pl.radomiej.emu.asm.pure.factory.PrintHelloWorldAsmExample;
 import pl.radomiej.emu.board.RaBoard;
+import pl.radomiej.emu.cpu.PmcCPU;
 import pl.radomiej.emu.cpu.PureCPU;
+import pl.radomiej.emu.logic.Optcode;
+import pl.radomiej.emu.logic.helpers.ToByteParser;
+import pl.radomiej.emu.logic.pure.PureByte;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 public class Intel8080BoardTest {
 
     @Test
-    public void bootloaderTest() {
+    public void bootloaderTest() throws IOException {
+        URL binPath = Resources.getResource("Intel8080/monitor.bin");
+        byte[] rawBin = Resources.toByteArray(binPath);
+        List<PureByte> pureBin = new ArrayList<>(rawBin.length);
+        Intel8080Babylon babylon = new Intel8080Babylon();
+        for(int i = 0; i < rawBin.length; i++){
+            char unsignedByte = (char) rawBin[i];
+            PureByte pureByte = ToByteParser.parse(unsignedByte);
+            pureBin.add(pureByte);
+        }
+
+        LinkedList<PureByte> pureStackBin = new LinkedList(pureBin);
+
+        Optcode<PmcCPU> optcode = babylon.createOptcodeFromMachineCode(pureStackBin);
+        System.out.println(optcode);
+
         PureCPU myCPU = new PureCPU(IncrementAsmExample.builder().build().createProgramData());
 
         long startTime = System.nanoTime();
